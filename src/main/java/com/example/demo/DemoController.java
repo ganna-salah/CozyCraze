e860522e351f41df8da6eda11503a643aa16fd2b
ganna-salah <gannasalah2003@gmail.com>
2023-12-15 22:45:11 +0200

    sbBegin

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -3,22 +3,44 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DemoController {
 
-    @GetMapping("/ganna")
-    public String showForm() {
-        return "trial";
+    @GetMapping("/showForm")
+    
+    public String showForm(Model model) {
+        model.addAttribute("formObject", new YourFormObject());
+        return "trial"; // Assuming Thymeleaf template name is "trail.html"
     }
 
     @PostMapping("/submitForm")
-    public String submitForm(@ModelAttribute Reg reg,Model model ) {
-        System.out.println(reg.toString());
-        return "trial";
+    public String submitForm(YourFormObject yourFormObject) {
+        System.out.println(yourFormObject.getFieldName());
+        System.out.println(yourFormObject.getAge());
+        return "home";
     }
-    
-    
+
+
+    public class YourFormObject {
+        private String fieldName;
+        private int age;
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getFieldName() {
+            return fieldName;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public void setFieldName(String fieldName) {
+            this.fieldName = fieldName;
+        }
+    }
 }
