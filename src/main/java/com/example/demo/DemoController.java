3af22b01d4016ed831fe9e2a3bdbf04b6e6b7a53
ganna-salah <gannasalah2003@gmail.com>
2023-12-15 14:01:36 +0200

    demo

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello() throws IOException {
+        // Load the content of the "home.html" file from the classpath
+        Resource resource = new ClassPathResource("static/home.html");
+        InputStreamReader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
+        try {
+            // Read the content of the file as a string
+            String content = FileCopyUtils.copyToString(reader);
+            return content;
+        } finally {
+            reader.close();
+        }
+    }
+}
